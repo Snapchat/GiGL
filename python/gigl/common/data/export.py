0f68493b66ec021fa9e4cba3299fdffe1866df6b
xgao4 <xgao4@snapchat.com>
2025-05-04 23:42:25 +0000

    Return BigQuery load job object

--- python/gigl/common/data/export.py
+++ python/gigl/common/data/export.py
@@ -16,6 +16,7 @@ import fastavro.types
 import requests
 import torch
 from google.cloud import bigquery
+from google.cloud.bigquery.job import LoadJob
 from google.cloud.exceptions import GoogleCloudError
 from typing_extensions import Self
 
@@ -215,7 +216,7 @@ class EmbeddingExporter:
 # TODO(kmonte): We should migrate this over to `BqUtils.load_files_to_bq` once that is implemented.
 def load_embeddings_to_bigquery(
     gcs_folder: GcsUri, project_id: str, dataset_id: str, table_id: str
-) -> None:
+) -> LoadJob:
     """
     Loads multiple Avro files containing GNN embeddings from GCS into BigQuery.
 
@@ -259,3 +260,5 @@ def load_embeddings_to_bigquery(
     logger.info(
         f"Loading {load_job.output_rows:,} rows into {dataset_id}:{table_id} in {time.perf_counter() - start:.2f} seconds."
     )
+
+    return load_job
