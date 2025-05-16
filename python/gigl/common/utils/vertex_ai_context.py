60027098629a8acfdd3fc329fcc203fe602e4db2
mkolodner <mkolodner@snapchat.com>
2025-05-16 18:28:24 +0000

    Update

--- python/gigl/common/utils/vertex_ai_context.py
+++ python/gigl/common/utils/vertex_ai_context.py
@@ -76,7 +76,9 @@ def get_leader_port() -> int:
 
 def get_local_world_size() -> int:
     """
-    The total number of processes spun up on each VAI Machine. This may be set upon launching a VAI job manually
+    The total number of processes spun up on each VAI Machine. This is currently is manually set upon launching a VAI job manually for GLT processes.
+    We should deprecate this in the future if we migrate VAI jobs to be spun up with torchrun instead.
+    Throws if not on Vertex AI.
     """
     if not is_currently_running_in_vertex_ai_job():
         raise _VAI_EXCEPTION
@@ -122,6 +124,7 @@ def connect_worker_pool() -> DistributedContext:
 
     global_rank = get_rank()
     global_world_size = get_world_size()
+    local_world_size = get_local_world_size()
 
     is_leader_worker = global_rank == 0
     ip_file_uri = GcsUri(_get_leader_worker_internal_ip_file_path())
