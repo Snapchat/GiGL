954a7c6baf02cb5c2c38fe937d89a3b5bc6fa61d
mkolodner <mkolodner@snapchat.com>
2025-06-12 00:05:00 +0000

    Update

--- python/tests/unit/distributed/distributed_neighborloader_test.py
+++ python/tests/unit/distributed/distributed_neighborloader_test.py
@@ -458,7 +458,6 @@ class DistributedNeighborLoaderTest(unittest.TestCase):
             ),
         ]
     )
-    @unittest.skip("Failing on Google Cloud Build - skiping for now")
     def test_ablp_dataloader(
         self,
         _,
@@ -525,7 +524,6 @@ class DistributedNeighborLoaderTest(unittest.TestCase):
             ),
         )
 
-    @unittest.skip("Failing on Google Cloud Build - skiping for now")
     def test_cora_supervised(self):
         cora_supervised_info = get_mocked_dataset_artifact_metadata()[
             CORA_USER_DEFINED_NODE_ANCHOR_MOCKED_DATASET_INFO.name
@@ -559,7 +557,6 @@ class DistributedNeighborLoaderTest(unittest.TestCase):
             fn=_run_cora_supervised, args=(dataset, self._context, expected_data_count)
         )
 
-    @unittest.skip("Failing on Google Cloud Build - skiping for now")
     def test_multiple_neighbor_loader(self):
         master_port = glt.utils.get_free_port(self._master_ip_address)
         expected_data_count = 2708
@@ -629,7 +626,6 @@ class DistributedNeighborLoaderTest(unittest.TestCase):
             param("Range-based partitioning", partitioner_class=DistRangePartitioner),
         ]
     )
-    @unittest.skip("Failing on Google Cloud Build - skiping for now")
     def test_toy_heterogeneous_ablp(self, _, partitioner_class: type[DistPartitioner]):
         toy_heterogeneous_supervised_info = get_mocked_dataset_artifact_metadata()[
             HETEROGENEOUS_TOY_GRAPH_NODE_ANCHOR_MOCKED_DATASET_INFO.name
