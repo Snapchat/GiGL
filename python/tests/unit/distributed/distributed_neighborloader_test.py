72f70f9f9ceb4dfa2607d0d9c07cb604eacdb44d
svij <svij@snapchat.com>
2025-06-26 07:52:33 +0000

    test

--- python/tests/unit/distributed/distributed_neighborloader_test.py
+++ python/tests/unit/distributed/distributed_neighborloader_test.py
@@ -9,6 +9,7 @@ from parameterized import param, parameterized
 from torch_geometric.data import Data, HeteroData
 
 import gigl.distributed.utils
+
 from gigl.distributed.dataset_factory import build_dataset
 from gigl.distributed.dist_ablp_neighborloader import DistABLPLoader
 from gigl.distributed.dist_context import DistributedContext
