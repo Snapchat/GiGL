2db823dfcc394c653483007ea60a4152e681c4fb
svij <svij@snapchat.com>
2025-06-30 20:41:28 +0000

    enable old test

--- python/tests/unit/utils/gcs_test.py
+++ python/tests/unit/utils/gcs_test.py
@@ -1,6 +1,8 @@
 import unittest
+from io import BytesIO
 from unittest.mock import MagicMock, patch
 
+from google.cloud.storage.blob import Blob
 from google.cloud.storage.bucket import Bucket
 from google.cloud.storage.client import Client
 
@@ -9,33 +11,33 @@ from gigl.common.utils.gcs import GcsUtils
 
 
 class TestGcsUtils(unittest.TestCase):
-    # @patch("gigl.common.utils.gcs.storage")
-    # def test_upload_from_filelike(self, mock_storage_client):
-    #     # Mock the GCS client, bucket, and blob
-    #     mock_client = MagicMock(spec=Client)
-    #     mock_bucket = MagicMock(spec=Bucket)
-    #     mock_blob = MagicMock(spec=Blob)
+    @patch("gigl.common.utils.gcs.storage")
+    def test_upload_from_filelike(self, mock_storage_client):
+        # Mock the GCS client, bucket, and blob
+        mock_client = MagicMock(spec=Client)
+        mock_bucket = MagicMock(spec=Bucket)
+        mock_blob = MagicMock(spec=Blob)
 
-    #     mock_storage_client.Client.return_value = mock_client
-    #     mock_client.bucket.return_value = mock_bucket
-    #     mock_bucket.blob.return_value = mock_blob
+        mock_storage_client.Client.return_value = mock_client
+        mock_client.bucket.return_value = mock_bucket
+        mock_bucket.blob.return_value = mock_blob
 
-    #     # Create a file-like object
-    #     filelike = BytesIO(b"test content")
+        # Create a file-like object
+        filelike = BytesIO(b"test content")
 
-    #     # Define GCS URI
-    #     gcs_uri = GcsUri("gs://test-bucket/test-path/test-file.txt")
+        # Define GCS URI
+        gcs_uri = GcsUri("gs://test-bucket/test-path/test-file.txt")
 
-    #     # Call the function
-    #     gcs_utils = GcsUtils()
-    #     gcs_utils.upload_from_filelike(gcs_uri, filelike)
+        # Call the function
+        gcs_utils = GcsUtils()
+        gcs_utils.upload_from_filelike(gcs_uri, filelike)
 
-    #     # Assertions
-    #     mock_client.bucket.assert_called_once_with("test-bucket")
-    #     mock_bucket.blob.assert_called_once_with("test-path/test-file.txt")
-    #     mock_blob.upload_from_file.assert_called_once_with(
-    #         filelike, content_type="application/octet-stream"
-    #     )
+        # Assertions
+        mock_client.bucket.assert_called_once_with("test-bucket")
+        mock_bucket.blob.assert_called_once_with("test-path/test-file.txt")
+        mock_blob.upload_from_file.assert_called_once_with(
+            filelike, content_type="application/octet-stream"
+        )
 
     def test_delete_files_in_bucket_dir(self):
         # Mock the GCS client, bucket, and blob
