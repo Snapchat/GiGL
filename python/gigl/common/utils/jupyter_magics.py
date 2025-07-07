ce3e13615c6723abca0c6028f6984ba2b58830c9
kmontemayor <kyle.e.montemayor@gmail.com>
2025-07-07 16:41:46 +0000

    sort imports

--- python/gigl/common/utils/jupyter_magics.py
+++ python/gigl/common/utils/jupyter_magics.py
@@ -1,18 +1,5 @@
 import os
 import pathlib
-
-gigl_root_dir = pathlib.Path(__file__).parent.parent.parent.parent.parent
-
-
-def change_working_dir_to_gigl_root():
-    """
-    Can be used inside notebooks to change the working directory to the GIGL root directory.
-    This is useful for ensuring that relative imports and file paths work correctly no matter where the notebook is located.
-    """
-    os.chdir(gigl_root_dir)
-    print(f"Changed working directory to: {gigl_root_dir}")
-
-
 from difflib import unified_diff
 from typing import Optional, Type, Union
 
@@ -26,6 +13,17 @@ from torch_geometric.data import HeteroData
 
 from gigl.common import Uri
 from snapchat.research.gbml import training_samples_schema_pb2
+
+gigl_root_dir = pathlib.Path(__file__).parent.parent.parent.parent.parent
+
+
+def change_working_dir_to_gigl_root():
+    """
+    Can be used inside notebooks to change the working directory to the GIGL root directory.
+    This is useful for ensuring that relative imports and file paths work correctly no matter where the notebook is located.
+    """
+    os.chdir(gigl_root_dir)
+    print(f"Changed working directory to: {gigl_root_dir}")
 
 
 class GraphVisualizer:
