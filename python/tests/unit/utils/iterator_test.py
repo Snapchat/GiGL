0ed18de9324942b599cc7a6be8fe7cab1388d253
mkolodner <mkolodner@snapchat.com>
2025-07-08 08:14:50 +0000

    Add test

--- python/tests/unit/utils/iterator_test.py
+++ python/tests/unit/utils/iterator_test.py
@@ -1,14 +1,16 @@
 import unittest
+from typing import Final
 
 from gigl.utils.iterator import InfiniteIterator
+
+_ITERABLE_LIST: Final[list[int]] = [3, 4, 5, 6]
 
 
 class IteratorTest(unittest.TestCase):
     def test_infinite_iterator(self):
-        iterable_list: list[int] = [3, 4, 5, 6]
         expected_list: list[int] = [3, 4, 5, 6, 3, 4, 5, 6, 3, 4]
         actual_list: list[int] = []
-        infinite_iterator = InfiniteIterator(iterable=iterable_list)
+        infinite_iterator = InfiniteIterator(iterable=_ITERABLE_LIST)
         count = 0
         for item in infinite_iterator:
             if count == len(expected_list):
@@ -16,6 +18,15 @@ class IteratorTest(unittest.TestCase):
             actual_list.append(item)
             count += 1
         self.assertEqual(actual_list, expected_list)
+
+    def test_infinite_iterator_shutdown(self):
+        infinite_iterator = InfiniteIterator(iterable=_ITERABLE_LIST)
+        for _ in infinite_iterator:
+            break
+        infinite_iterator.shutdown()
+        with self.assertRaises(ValueError):
+            for _ in infinite_iterator:
+                break
 
 
 if __name__ == "__main__":
