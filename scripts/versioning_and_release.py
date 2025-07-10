3ab98f03480921687d988acad0be7009ca6da054
Shubham Vij <svij@snapchat.com>
2025-07-09 18:38:23 -0700

    add time to nightly tag
    Co-authored-by: Shubham Vij <reachme@shubhamvij.com>

--- scripts/versioning_and_release.py
+++ scripts/versioning_and_release.py
@@ -134,7 +134,7 @@ def get_new_version(
         patch += 1
     new_version = f"{major}.{minor}.{patch}"
     if bump_type == "nightly":
-        new_version += f"-nightly.{datetime.datetime.now().strftime('%Y%m%d')}"
+        new_version += f"-nightly.{datetime.datetime.now().strftime('%Y%m%d.%H%M%S')}"
     return new_version
 
 
