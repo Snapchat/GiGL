93a92649aaefb365a19183e1e3d25226c6031b6e
kmontemayor <kyle.e.montemayor@gmail.com>
2025-07-17 22:12:28 +0000

    use default machine size

--- python/gigl/orchestration/kubeflow/utils/resource.py
+++ python/gigl/orchestration/kubeflow/utils/resource.py
@@ -18,8 +18,8 @@ def add_task_resource_requirements(
         None
     """
     DEFAULT_CPU_REQUEST = "4"
-    DEFAULT_MEMORY_REQUEST = "1Gi"
+    DEFAULT_MEMORY_REQUEST = "16G"
     # default to cpu image, overwrite later as needed
     task.container_spec.image = common_pipeline_component_configs.cpu_container_image
-    task.set_cpu_request(DEFAULT_CPU_REQUEST)
-    task.set_memory_request(DEFAULT_MEMORY_REQUEST)
+    # task.set_cpu_request(DEFAULT_CPU_REQUEST)
+    # task.set_memory_limit(DEFAULT_MEMORY_REQUEST)
