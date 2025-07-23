6b3b65d55e86eabe2283ddc90fc004dd1020f20d
Shubham Vij <svij@snapchat.com>
2025-07-23 09:21:27 -0700

    comments
    Co-authored-by: Shubham Vij <reachme@shubhamvij.com>

--- requirements/install_scala_deps.sh
+++ requirements/install_scala_deps.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# This script is used to download and install the scala dependencies for the project.
+# The script exposes a --download-only flag to only download the files, and skip the installation and setup.
+# This is useful when building a docker image, for when we want the host context to have all the scala deps
+# so we can copy over the deps from the host into the built docker image, without mounting and credentials
+# to the docker build - which can be prone to danger. Currently, our hosted GCS assets have:
+# - allAuthenticatedUsers read access, thus one needs to be authenticated w/ GCP to download the files.
+
 set -e
 
 # Parse command line arguments
@@ -53,32 +60,36 @@ download_if_missing() {
     fi
 }
 
-echo "Creating required directories"
-mkdir -p "$TOOLS_COURSIER_DIR"
-mkdir -p "$TOOLS_SCALAPBC_DIR"
-mkdir -p "$TOOLS_SCALA_DIR"
 
-echo "Downloading all required files..."
+download_all_scala_deps() {
+    echo "Creating required directories"
+    mkdir -p "$TOOLS_COURSIER_DIR"
+    mkdir -p "$TOOLS_SCALAPBC_DIR"
+    mkdir -p "$TOOLS_SCALA_DIR"
 
-# Download coursier for Linux (Mac uses brew), or if running in download-only mode
-if ! is_running_on_mac || [ "$DOWNLOAD_ONLY" = true ]; then
-    download_if_missing "gs://public-gigl/tools/scala/coursier/cs-x86_64-pc-linux.gz" "$COURSIER_GZ_LOCAL_PATH"
-fi
+    echo "Downloading all required files..."
 
-# Download Spark distributions
-download_if_missing "gs://public-gigl/tools/scala/spark/spark-3.1.3-bin-hadoop3.2.tgz" "$SPARK_31_TAR_LOCAL_PATH"
-download_if_missing "gs://public-gigl/tools/scala/spark/spark-3.5.0-bin-hadoop3.tgz" "$SPARK_35_TAR_LOCAL_PATH"
+    # Download coursier for Linux (Mac uses brew), or if running in download-only mode
+    if ! is_running_on_mac || [ "$DOWNLOAD_ONLY" = true ]; then
+        download_if_missing "gs://public-gigl/tools/scala/coursier/cs-x86_64-pc-linux.gz" "$COURSIER_GZ_LOCAL_PATH"
+    fi
 
-# Download TFRecord JARs
-download_if_missing "$SPARK_31_TFRECORD_JAR_GCS_PATH" "$SPARK_31_TFRECORD_JAR_LOCAL_PATH"
-download_if_missing "$SPARK_35_TFRECORD_JAR_GCS_PATH" "$SPARK_35_TFRECORD_JAR_LOCAL_PATH"
+    # Download Spark distributions
+    download_if_missing "gs://public-gigl/tools/scala/spark/spark-3.1.3-bin-hadoop3.2.tgz" "$SPARK_31_TAR_LOCAL_PATH"
+    download_if_missing "gs://public-gigl/tools/scala/spark/spark-3.5.0-bin-hadoop3.tgz" "$SPARK_35_TAR_LOCAL_PATH"
 
-# Download ScalaPB distributions
-download_if_missing "gs://public-gigl/tools/scala/scalapbc/scalapbc-0.11.11.zip" "$SCALAPB_0_11_11_LOCAL_PATH"
-download_if_missing "gs://public-gigl/tools/scala/scalapbc/scalapbc-0.11.14.zip" "$SCALAPB_0_11_14_LOCAL_PATH"
+    # Download TFRecord JARs
+    download_if_missing "$SPARK_31_TFRECORD_JAR_GCS_PATH" "$SPARK_31_TFRECORD_JAR_LOCAL_PATH"
+    download_if_missing "$SPARK_35_TFRECORD_JAR_GCS_PATH" "$SPARK_35_TFRECORD_JAR_LOCAL_PATH"
 
-echo "All downloads completed successfully"
+    # Download ScalaPB distributions
+    download_if_missing "gs://public-gigl/tools/scala/scalapbc/scalapbc-0.11.11.zip" "$SCALAPB_0_11_11_LOCAL_PATH"
+    download_if_missing "gs://public-gigl/tools/scala/scalapbc/scalapbc-0.11.14.zip" "$SCALAPB_0_11_14_LOCAL_PATH"
 
+    echo "All downloads completed successfully"
+}
+
+download_all_scala_deps
 # Exit if download-only flag is set
 if [ "$DOWNLOAD_ONLY" = true ]; then
     echo "Download-only mode: Skipping installation and setup steps"
@@ -107,19 +118,18 @@ gunzip -c "$SPARK_31_TAR_LOCAL_PATH" | tar xopf - -C "$TOOLS_SCALA_DIR"
 echo "Installing tooling for spark 3.5; this will deprecate regular installation for spark 3.1 above"
 gunzip -c "$SPARK_35_TAR_LOCAL_PATH" | tar xopf - -C "$TOOLS_SCALA_DIR"
 
+# Remove SparkR, as it contains GPL licensed code - even though Spark is Apache licensed
+# We don't use this anyways.
+rm -rf "$TOOLS_SCALA_DIR/spark-3.1.3-bin-hadoop3.2/R"
+rm -rf "$TOOLS_SCALA_DIR/spark-3.5.0-bin-hadoop3/R"
+
+
 echo "Installing tooling for scala protobuf"
-# Commenting out as we are seeing some issues in the builders downloading this from github
-# curl -L -o tools/scalapbc/scalapbc.zip "https://github.com/scalapb/ScalaPB/releases/download/v0.11.11/scalapbc-0.11.11.zip"
 unzip -o "$SCALAPB_0_11_11_LOCAL_PATH" -d "$TOOLS_SCALAPBC_DIR"
 rm "$SCALAPB_0_11_11_LOCAL_PATH"
 # (svij-sc) scala35 support (this will eventually deprecate above)
 unzip -o "$SCALAPB_0_11_14_LOCAL_PATH" -d "$TOOLS_SCALAPBC_DIR"
 rm "$SCALAPB_0_11_14_LOCAL_PATH"
-
-# Remove SparkR, as it contains GPL licensed code - even though Spark is Apache licensed
-# We don't use this anyways.
-rm -rf "$TOOLS_SCALA_DIR/spark-3.1.3-bin-hadoop3.2/R"
-rm -rf "$TOOLS_SCALA_DIR/spark-3.5.0-bin-hadoop3/R"
 
 # if running into the following error when running unittest locally
 # `java.lang.NoClassDefFoundError: Could not initialize class org.apache.spark.storage.StorageUtils$`
