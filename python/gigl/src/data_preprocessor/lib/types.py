10a5ee46cec183158452a00d01a2c70a48b8b859
kmontemayor <kyle.e.montemayor@gmail.com>
2025-08-15 20:46:11 +0000

    add commenets

--- python/gigl/src/data_preprocessor/lib/types.py
+++ python/gigl/src/data_preprocessor/lib/types.py
@@ -11,6 +11,14 @@ from gigl.common import Uri
 
 # TODO (mkolodner-sc): Move these variables to a more general location, as they are used even outside of context of data preprocessor
 
+# Note: we use `typing.Dict` here instead of `dict` as the version of Beam we are on (2.56.0)
+# will see some failures when running with very large data and `dict`.
+# I am not sure what the root cause here is,
+# but I suspect it may be related to our version of Beam only parsing `typing` annotations [1]
+# and dicts having some special Coder [2].
+# If/when we upgrade beam versions, we could revisit this.
+# [1]: https://github.com/apache/beam/blob/96d2a281cd69698463d5455861335b4d7d09e795/sdks/python/apache_beam/typehints/native_type_compatibility.py#L277-L279
+# [2]: https://github.com/apache/beam/blob/96d2a281cd69698463d5455861335b4d7d09e795/sdks/python/apache_beam/coders/typecoders.py#L96
 InstanceDict = Dict[str, Any]
 TFTensorDict = Dict[str, common_types.TensorType]
 FeatureSpecDict = Dict[str, common_types.FeatureSpecType]
