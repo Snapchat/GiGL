1049c3863d79c85efd9ce5d396348ddec9b1fb3f
mkolodner-sc <mkolodner@snapchat.com>
2025-09-09 16:26:40 -0700

    Deprecate use of DistLinkPredictionDataset in GiGL in favor of DistDataset (#310)

--- python/gigl/distributed/dataset_factory.py
+++ python/gigl/distributed/dataset_factory.py
@@ -1,5 +1,5 @@
 """
-DatasetFactory is responsible for building and returning a DistLinkPredictionDataset class or subclass. It does this by spawning a
+DatasetFactory is responsible for building and returning a DistDataset class or subclass. It does this by spawning a
 process which initializes rpc + worker group, loads and builds a partitioned dataset, and shuts down the rpc + worker group.
 """
 import time
@@ -29,7 +29,7 @@ from gigl.common.logger import Logger
 from gigl.common.utils.decorator import tf_on_cpu
 from gigl.distributed.constants import DEFAULT_MASTER_DATA_BUILDING_PORT
 from gigl.distributed.dist_context import DistributedContext
-from gigl.distributed.dist_link_prediction_dataset import DistLinkPredictionDataset
+from gigl.distributed.dist_dataset import DistDataset
 from gigl.distributed.dist_partitioner import DistPartitioner
 from gigl.distributed.dist_range_partitioner import DistRangePartitioner
 from gigl.distributed.utils import (
@@ -195,9 +195,9 @@ def _load_and_build_partitioned_dataset(
     edge_tf_dataset_options: TFDatasetOptions,
     splitter: Optional[Union[NodeSplitter, NodeAnchorLinkSplitter]] = None,
     _ssl_positive_label_percentage: Optional[float] = None,
-) -> DistLinkPredictionDataset:
+) -> DistDataset:
     """
-    Given some information about serialized TFRecords, loads and builds a partitioned dataset into a DistLinkPredictionDataset class.
+    Given some information about serialized TFRecords, loads and builds a partitioned dataset into a DistDataset class.
     We require init_rpc and init_worker_group have been called to set up the rpc and context, respectively, prior to calling this function. If this is not
     set up beforehand, this function will throw an error.
     Args:
@@ -212,7 +212,7 @@ def _load_and_build_partitioned_dataset(
         _ssl_positive_label_percentage (Optional[float]): Percentage of edges to select as self-supervised labels. Must be None if supervised edge labels are provided in advance.
             Slotted for refactor once this functionality is available in the transductive `splitter` directly
     Returns:
-        DistLinkPredictionDataset: Initialized dataset with partitioned graph information
+        DistDataset: Initialized dataset with partitioned graph information
 
     """
     assert (
@@ -341,12 +341,8 @@ def _load_and_build_partitioned_dataset(
         else serialized_graph_metadata.node_entity_info.feature_dim,
     )
 
-    logger.info(
-        f"Initializing DistLinkPredictionDataset instance with edge direction {edge_dir}"
-    )
-    dataset = DistLinkPredictionDataset(
-        rank=rank, world_size=world_size, edge_dir=edge_dir
-    )
+    logger.info(f"Initializing DistDataset instance with edge direction {edge_dir}")
+    dataset = DistDataset(rank=rank, world_size=world_size, edge_dir=edge_dir)
 
     dataset.build(
         partition_output=partition_output,
@@ -358,7 +354,7 @@ def _load_and_build_partitioned_dataset(
 
 def _build_dataset_process(
     process_number_on_current_machine: int,
-    output_dict: MutableMapping[str, DistLinkPredictionDataset],
+    output_dict: MutableMapping[str, DistDataset],
     serialized_graph_metadata: SerializedGraphMetadata,
     master_ip_address: str,
     master_dataset_building_ports: Tuple[int, int],
@@ -377,7 +373,7 @@ def _build_dataset_process(
         1. Initializing worker group and rpc connections
         2. Loading Torch tensors from serialized TFRecords
         3. Partition loaded Torch tensors across multiple machines
-        4. Loading and formatting graph and feature partition data into a `DistLinkPredictionDataset` class, which will be used during inference
+        4. Loading and formatting graph and feature partition data into a `DistDataset` class, which will be used during inference
         5. Tearing down these connections
     Steps 2-4 are done by the `load_and_build_partitioned_dataset` function.
 
@@ -391,7 +387,7 @@ def _build_dataset_process(
     Args:
         process_number_on_current_machine (int): Process number on current machine. This parameter is required and provided by mp.spawn.
             This is always set to 1 for dataset building.
-        output_dict (MutableMapping[str, DistLinkPredictionDataset]): A dictionary spawned by a mp.manager which the built dataset
+        output_dict (MutableMapping[str, DistDataset]): A dictionary spawned by a mp.manager which the built dataset
             will be written to for use by the parent process
         serialized_graph_metadata (SerializedGraphMetadata): Metadata about TFRecords that are serialized to disk
         master_ip_address (str): IP address of the master node
@@ -443,7 +439,7 @@ def _build_dataset_process(
             rank=node_rank,
         )
 
-    output_dataset: DistLinkPredictionDataset = _load_and_build_partitioned_dataset(
+    output_dataset: DistDataset = _load_and_build_partitioned_dataset(
         serialized_graph_metadata=serialized_graph_metadata,
         should_load_tensors_in_parallel=should_load_tensors_in_parallel,
         edge_dir=sample_edge_direction,
@@ -477,9 +473,9 @@ def build_dataset(
     _dataset_building_port: Optional[
         int
     ] = None,  # WARNING: This field will be deprecated in the future
-) -> DistLinkPredictionDataset:
+) -> DistDataset:
     """
-    Launches a spawned process for building and returning a DistLinkPredictionDataset instance provided some
+    Launches a spawned process for building and returning a DistDataset instance provided some
     SerializedGraphMetadata.
 
     It is expected that there is only one `build_dataset` call per node (machine).
@@ -511,7 +507,7 @@ def build_dataset(
             be initialized from the current torch.distributed context.
 
     Returns:
-        DistLinkPredictionDataset: Built GraphLearn-for-PyTorch Dataset class
+        DistDataset: Built GraphLearn-for-PyTorch Dataset class
     """
     if distributed_context is not None:
         logger.warning(
@@ -605,7 +601,7 @@ def build_dataset(
         ),
     )
 
-    output_dataset: DistLinkPredictionDataset = output_dict["dataset"]
+    output_dataset: DistDataset = output_dict["dataset"]
 
     logger.info(
         f"Dataset Building finished on rank {node_rank} of {node_world_size}, which took {time.time()-dataset_building_start_time:.2f} seconds"
@@ -619,7 +615,7 @@ def build_dataset_from_task_config_uri(
     distributed_context: Optional[DistributedContext] = None,
     is_inference: bool = True,
     _tfrecord_uri_pattern: str = ".*-of-.*\.tfrecord(\.gz)?$",
-) -> DistLinkPredictionDataset:
+) -> DistDataset:
     """
     Builds a dataset from a provided `task_config_uri` as part of GiGL orchestration. Parameters to
     this step should be provided in the `inferenceArgs` field of the GbmlConfig for inference or the
