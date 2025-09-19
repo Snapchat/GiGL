877f01f242cc0a9eaafdb0c4707c20e662f7b873
kmontemayor <kyle.e.montemayor@gmail.com>
2025-09-19 23:51:14 +0000

    fix

--- python/tests/unit/distributed/dist_ablp_neighborloader_test.py
+++ python/tests/unit/distributed/dist_ablp_neighborloader_test.py
@@ -396,7 +396,7 @@ class DistABLPLoaderTest(unittest.TestCase):
             ),
         )
 
-    def _test_cora_supervised(self):
+    def test_cora_supervised(self):
         cora_supervised_info = get_mocked_dataset_artifact_metadata()[
             CORA_USER_DEFINED_NODE_ANCHOR_MOCKED_DATASET_INFO.name
         ]
@@ -438,7 +438,7 @@ class DistABLPLoaderTest(unittest.TestCase):
 
     # TODO: (mkolodner-sc) - Figure out why this test is failing on Google Cloud Build
     @unittest.skip("Failing on Google Cloud Build - skiping for now")
-    def _test_dblp_supervised(self):
+    def test_dblp_supervised(self):
         dblp_supervised_info = get_mocked_dataset_artifact_metadata()[
             DBLP_GRAPH_NODE_ANCHOR_MOCKED_DATASET_INFO.name
         ]
@@ -506,7 +506,7 @@ class DistABLPLoaderTest(unittest.TestCase):
             ),
         ]
     )
-    def _test_toy_heterogeneous_ablp(
+    def test_toy_heterogeneous_ablp(
         self,
         _,
         partitioner_class: type[DistPartitioner],
