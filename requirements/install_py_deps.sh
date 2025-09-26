87ed2eb79eb2a989a4fa7c56ceea92b344dd4e1c
mkolodner <mkolodner@snapchat.com>
2025-09-26 18:59:57 +0000

    Update

--- requirements/install_py_deps.sh
+++ requirements/install_py_deps.sh
@@ -84,10 +84,7 @@ echo "Installing from ${req_file}"
 pip install -r $req_file $PIP_ARGS
 
 
-# Taken from https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script
-# We do this so if `install_py_deps.sh` is run from a different directory, the script can still find the post_install.py file.
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-python $SCRIPT_DIR/../python/gigl/scripts/post_install.py
+python python/gigl/scripts/post_install.py
 
 # TODO: (svij) Check if gperftools is still needed
 # https://github.com/Snapchat/GiGL/issues/296
