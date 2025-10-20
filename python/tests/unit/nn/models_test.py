39e1d6d4dd2a55ac99ae2a9e329627359050a221
swong3 <swong3@snapchat.com>
2025-10-20 20:30:20 +0000

    Added larger world size test

--- python/tests/unit/nn/models_test.py
+++ python/tests/unit/nn/models_test.py
@@ -6,6 +6,7 @@ import torch.distributed as dist
 import torch.multiprocessing as mp
 import torch.nn as nn
 import torch.distributed as dist
+import torch.multiprocessing as mp
 from torch_geometric.data import Data, HeteroData
 from torch_geometric.nn.models import LightGCN as PyGLightGCN
 from torchrec.distributed.model_parallel import (
