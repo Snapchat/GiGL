b8d321b427ddaee076a75f058b12076697434fc0
kmontemayor <kyle.e.montemayor@gmail.com>
2025-11-04 21:23:54 +0000

    add deprecation logs

--- python/gigl/module/loss.py
+++ python/gigl/module/loss.py
@@ -1,3 +1,11 @@
+from gigl.common.logger import Logger
 from gigl.nn.loss import RetrievalLoss
 
 __all__ = ["RetrievalLoss"]
+
+logger = Logger()
+
+logger.warning(
+    "gigl.module.loss is deprecated and will be removed in a future release. "
+    "Please use the `gigl.nn.loss` module instead."
+)
--- python/gigl/module/models.py
+++ python/gigl/module/models.py
@@ -1,3 +1,11 @@
+from gigl.common.logger import Logger
 from gigl.nn.models import LightGCN, LinkPredictionGNN
 
 __all__ = ["LinkPredictionGNN", "LightGCN"]
+
+logger = Logger()
+
+logger.warning(
+    "gigl.module.models is deprecated and will be removed in a future release. "
+    "Please use the `gigl.nn.models` module instead."
+)
