7f6819ff430748d47922be29e46799aa0872b281
Kyle Montemayor <kmontemayor2@snapchat.com>
2025-12-04 08:37:38 -0800

    Support launching graph store jobs from trainer and inferencer (#390)
    Co-authored-by: kmontemayor <kyle.e.montemayor@gmail.com>

--- python/gigl/src/inference/v2/glt_inferencer.py
+++ python/gigl/src/inference/v2/glt_inferencer.py
@@ -1,15 +1,8 @@
 import argparse
 from typing import Optional
 
-from google.cloud.aiplatform_v1.types import Scheduling, accelerator_type, env_var
-
 from gigl.common import Uri, UriFactory
-from gigl.common.constants import (
-    DEFAULT_GIGL_RELEASE_SRC_IMAGE_CPU,
-    DEFAULT_GIGL_RELEASE_SRC_IMAGE_CUDA,
-)
 from gigl.common.logger import Logger
-from gigl.common.services.vertex_ai import VertexAiJobConfig, VertexAIService
 from gigl.env.pipelines_config import get_resource_config
 from gigl.src.common.constants.components import GiGLComponents
 from gigl.src.common.types import AppliedTaskIdentifier
@@ -18,30 +11,17 @@ from gigl.src.common.types.pb_wrappers.gigl_resource_config import (
     GiglResourceConfigWrapper,
 )
 from gigl.src.common.utils.metrics_service_provider import initialize_metrics
+from gigl.src.common.vertex_ai_launcher import (
+    launch_graph_store_enabled_job,
+    launch_single_pool_job,
+)
 from snapchat.research.gbml.gigl_resource_config_pb2 import (
     LocalResourceConfig,
+    VertexAiGraphStoreConfig,
     VertexAiResourceConfig,
 )
 
 logger = Logger()
-
-
-# TODO: (svij) This function may need some work cc @zfan3, @xgao4
-# i.e. dataloading may happen on gpu instead of inference. Curretly, there is no
-# great support for gpu data loading, thus we assume inference is done on gpu and
-# data loading is done on cpu. This will need to be revisited.
-def _determine_if_cpu_inference(
-    inferencer_resource_config: VertexAiResourceConfig,
-) -> bool:
-    """Determine whether CPU inference is required based on the glt_inferencer configuration."""
-    if (
-        not inferencer_resource_config.gpu_type
-        or inferencer_resource_config.gpu_type
-        == accelerator_type.AcceleratorType.ACCELERATOR_TYPE_UNSPECIFIED.name  # type: ignore[attr-defined] # `name` is defined
-    ):
-        return True
-    else:
-        return False
 
 
 class GLTInferencer:
@@ -74,73 +54,44 @@ class GLTInferencer:
         inference_process_runtime_args = (
             gbml_config_pb_wrapper.inferencer_config.inferencer_args
         )
-        assert isinstance(
+
+        job_name = f"gigl_infer_{applied_task_identifier}"
+
+        if isinstance(
             resource_config_wrapper.inferencer_config, VertexAiResourceConfig
-        )
-        inferencer_resource_config: VertexAiResourceConfig = (
-            resource_config_wrapper.inferencer_config
-        )
-
-        is_cpu_training = _determine_if_cpu_inference(
-            inferencer_resource_config=inferencer_resource_config
-        )
-        cpu_docker_uri = cpu_docker_uri or DEFAULT_GIGL_RELEASE_SRC_IMAGE_CPU
-        cuda_docker_uri = cuda_docker_uri or DEFAULT_GIGL_RELEASE_SRC_IMAGE_CUDA
-        container_uri = cpu_docker_uri if is_cpu_training else cuda_docker_uri
-
-        job_args = (
-            [
-                f"--job_name={applied_task_identifier}",
-                f"--task_config_uri={task_config_uri}",
-                f"--resource_config_uri={resource_config_uri}",
-            ]
-            + ([] if is_cpu_training else ["--use_cuda"])
-            + ([f"--{k}={v}" for k, v in inference_process_runtime_args.items()])
-        )
-
-        command = inference_process_command.strip().split(" ")
-        logger.info(f"Running inference with command: {command}")
-        vai_job_name = f"gigl_infer_{applied_task_identifier}"
-        environment_variables: list[env_var.EnvVar] = [
-            env_var.EnvVar(name="TF_CPP_MIN_LOG_LEVEL", value="3"),
-        ]
-        job_config = VertexAiJobConfig(
-            job_name=vai_job_name,
-            container_uri=container_uri,
-            command=command,
-            args=job_args,
-            environment_variables=environment_variables,
-            machine_type=inferencer_resource_config.machine_type,
-            accelerator_type=inferencer_resource_config.gpu_type.upper().replace(
-                "-", "_"
-            ),
-            accelerator_count=inferencer_resource_config.gpu_limit,
-            replica_count=inferencer_resource_config.num_replicas,
-            labels=resource_config_wrapper.get_resource_labels(
-                component=GiGLComponents.Inferencer
-            ),
-            timeout_s=inferencer_resource_config.timeout
-            if inferencer_resource_config.timeout
-            else None,
-            # This should be `aiplatform.gapic.Scheduling.Strategy[inferencer_resource_config.scheduling_strategy]`
-            # But mypy complains otherwise...
-            # python/gigl/src/inference/v2/glt_inferencer.py:124: error: The type "type[Strategy]" is not generic and not indexable  [misc]
-            # TODO(kmonte): Fix this
-            scheduling_strategy=getattr(
-                Scheduling.Strategy,
-                inferencer_resource_config.scheduling_strategy,
+        ):
+            launch_single_pool_job(
+                vertex_ai_resource_config=resource_config_wrapper.inferencer_config,
+                job_name=job_name,
+                task_config_uri=task_config_uri,
+                resource_config_uri=resource_config_uri,
+                process_command=inference_process_command,
+                process_runtime_args=inference_process_runtime_args,
+                resource_config_wrapper=resource_config_wrapper,
+                cpu_docker_uri=cpu_docker_uri,
+                cuda_docker_uri=cuda_docker_uri,
+                component=GiGLComponents.Inferencer,
+                vertex_ai_region=resource_config_wrapper.vertex_ai_inferencer_region,
             )
-            if inferencer_resource_config.scheduling_strategy
-            else None,
-        )
-
-        vertex_ai_service = VertexAIService(
-            project=resource_config_wrapper.project,
-            location=resource_config_wrapper.vertex_ai_inferencer_region,
-            service_account=resource_config_wrapper.service_account_email,
-            staging_bucket=resource_config_wrapper.temp_assets_regional_bucket_path.uri,
-        )
-        vertex_ai_service.launch_job(job_config=job_config)
+        elif isinstance(
+            resource_config_wrapper.inferencer_config, VertexAiGraphStoreConfig
+        ):
+            launch_graph_store_enabled_job(
+                vertex_ai_graph_store_config=resource_config_wrapper.inferencer_config,
+                job_name=job_name,
+                task_config_uri=task_config_uri,
+                resource_config_uri=resource_config_uri,
+                process_command=inference_process_command,
+                process_runtime_args=inference_process_runtime_args,
+                resource_config_wrapper=resource_config_wrapper,
+                cpu_docker_uri=cpu_docker_uri,
+                cuda_docker_uri=cuda_docker_uri,
+                component=GiGLComponents.Inferencer,
+            )
+        else:
+            raise NotImplementedError(
+                f"Unsupported resource config for glt inference: {type(resource_config_wrapper.inferencer_config).__name__}"
+            )
 
     def run(
         self,
@@ -157,10 +108,12 @@ class GLTInferencer:
 
         if isinstance(resource_config_wrapper.inferencer_config, LocalResourceConfig):
             raise NotImplementedError(
-                f"Local GLT Inferencer is not yet supported, please specify a {VertexAiResourceConfig.__name__} resource config field."
+                f"Local GLT Inferencer is not yet supported, please specify a {VertexAiResourceConfig.__name__} or {VertexAiGraphStoreConfig.__name__} resource config field."
             )
         elif isinstance(
             resource_config_wrapper.inferencer_config, VertexAiResourceConfig
+        ) or isinstance(
+            resource_config_wrapper.inferencer_config, VertexAiGraphStoreConfig
         ):
             self.__execute_VAI_inference(
                 applied_task_identifier=applied_task_identifier,
