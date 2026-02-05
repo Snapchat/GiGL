8c73483c998199488b774034c2062e72c5f19359
kmontemayor <kyle.e.montemayor@gmail.com>
2026-02-05 00:19:54 +0000

    remove

--- gigl/distributed/graph_store/storage_utils.py
+++
@@ -1,7 +0,0 @@
-"""Utils for operating on a dataset remotely.
-
-DEPRECATED: The functions in this module have been migrated to `DistServer` methods.
-Use `gigl.distributed.dist_server.DistServer` instead.
-
-This file is kept for backwards compatibility and will be removed in a future release.
-"""
