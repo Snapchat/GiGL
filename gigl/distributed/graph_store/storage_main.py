efc6f7b245e47945b1c2f87f34b0df0b35d8f876
Kyle Montemayor <kmontemayor2@snapchat.com>
2026-02-05 11:44:30 -0800

    Add e2e heterogeneous graph store inference example (#452)
    Co-authored-by: kmontemayor <kyle.e.montemayor@gmail.com>

--- gigl/distributed/graph_store/storage_main.py
+++ gigl/distributed/graph_store/storage_main.py
@@ -130,27 +130,46 @@ def storage_node_process(
         splitter=splitter,
         _ssl_positive_label_percentage=ssl_positive_label_percentage,
     )
-    torch_process_port = get_free_ports_from_master_node(num_ports=1)[0]
+    task_config = GbmlConfigPbWrapper.get_gbml_config_pb_wrapper_from_uri(
+        gbml_config_uri=task_config_uri
+    )
+    inference_node_types = sorted(
+        task_config.task_metadata_pb_wrapper.get_task_root_node_types()
+    )
+    logger.info(f"Inference node types: {inference_node_types}")
+    torch_process_ports = get_free_ports_from_master_node(
+        num_ports=len(inference_node_types)
+    )
     torch.distributed.destroy_process_group()
-    server_processes: list[py_mp_context.SpawnProcess] = []
     mp_context = torch.multiprocessing.get_context("spawn")
-    # TODO(kmonte): Enable more than one server process per machine
-    for i in range(1):
-        server_process = mp_context.Process(
-            target=_run_storage_process,
-            args=(
-                storage_rank + i,  # storage_rank
-                cluster_info,  # cluster_info
-                dataset,  # dataset
-                torch_process_port,  # torch_process_port
-                storage_world_backend,  # storage_world_backend
-            ),
+    # Since we create a new inference process for each inference node type, we need to start a new server process for each inference node type.
+    # We do this as you cannot re-connect to the same server process after it has been joined.
+    for i, inference_node_type in enumerate(inference_node_types):
+        logger.info(
+            f"Starting storage node for inference node type {inference_node_type} (storage process group {i} / {len(inference_node_types)})"
         )
-        server_processes.append(server_process)
-    for server_process in server_processes:
-        server_process.start()
-    for server_process in server_processes:
-        server_process.join(timeout_seconds)
+        server_processes: list[py_mp_context.SpawnProcess] = []
+        # TODO(kmonte): Enable more than one server process per machine
+        num_server_processes = 1
+        for i in range(num_server_processes):
+            server_process = mp_context.Process(
+                target=_run_storage_process,
+                args=(
+                    storage_rank + i,  # storage_rank
+                    cluster_info,  # cluster_info
+                    dataset,  # dataset
+                    torch_process_ports[i],  # torch_process_port
+                    storage_world_backend,  # storage_world_backend
+                ),
+            )
+            server_processes.append(server_process)
+            for server_process in server_processes:
+                server_process.start()
+            for server_process in server_processes:
+                server_process.join()
+            logger.info(
+                f"All server processes for inference node type {inference_node_type} joined"
+            )
 
 
 if __name__ == "__main__":
