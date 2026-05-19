6591d5255bad3a815031c461a30c64317273b40e
kmontemayor <kyle.e.montemayor@gmail.com>
2026-05-19 20:42:33 +0000

    remove

--- gigl/utils/dev/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Developer utilities (smoke entrypoints, ad-hoc test helpers).
-
-Modules under this package are intended for short, ad-hoc test jobs and
-developer iteration. They are NOT part of GiGL's stable public API.
-"""
