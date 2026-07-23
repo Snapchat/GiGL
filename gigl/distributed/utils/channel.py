fb44a2aa2baca7d3fd346b537450f7133cf15298
jchmura <jchmura@snapchat.com>
2026-07-23 17:13:52 +0000

    wip

--- gigl/distributed/utils/channel.py
+++ gigl/distributed/utils/channel.py
@@ -1,3 +1,4 @@
+import multiprocessing
 from graphlearn_torch.channel import SampleMessage, ShmChannel
 
 from gigl.src.common.utils.metrics_service_provider import (
@@ -5,51 +6,37 @@ from gigl.src.common.utils.metrics_service_provider import (
 )
 
 
-#class MonitoredShmChannel(ShmChannel):
-#    """Monitored variant of ShmChannel that automatically records enqueue/dequeue metrics."""
-#
-#    def __init__(self, channel_name: str, *args, **kwargs) -> None:
-#        super().__init__(*args, **kwargs)
-#        self._enqueued_metric = f"{channel_name}.enqueued"
-#        self._dequeued_metric = f"{channel_name}.dequeued"
-#
-#    def send(self, msg: SampleMessage, **kwargs) -> None:
-#        super().send(msg, **kwargs)
-#        publisher = get_metrics_service_instance()
-#        publisher.add_count(self._enqueued_metric, 1)
-#
-#    def recv(self, *args, **kwargs) -> SampleMessage:
-#        msg = super().recv(*args, **kwargs)
-#        publisher = get_metrics_service_instance()
-#        publisher.add_count(self._dequeued_metric, 1)
-#        return msg
-#
-import multiprocessing
-
-
 class MonitoredShmChannel(ShmChannel):
+    """Monitored variant of ShmChannel that automatically records enqueue/dequeue metrics."""
+
     def __init__(self, channel_name: str, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self._metric = f"{channel_name}.queue_size"
-        self._shared_qsize = multiprocessing.Value("i", 0)
+        self._enqueued_metric = f"{channel_name}.enqueued"
+        self._dequeued_metric = f"{channel_name}.dequeued"
+
+        #self._metric = f"{channel_name}.queue_size"
+        #self._shared_qsize = multiprocessing.Value("i", 0)
 
     def send(self, msg: SampleMessage, **kwargs) -> None:
         super().send(msg, **kwargs)
-
-        with self._shared_qsize.get_lock():
-            self._shared_qsize.value += 1
-            qsize = self._shared_qsize.value
-
         publisher = get_metrics_service_instance()
-        publisher.add_gauge(self._metric, qsize)
+        publisher.add_count(self._enqueued_metric, 1)
+
+        #with self._shared_qsize.get_lock():
+        #    self._shared_qsize.value += 1
+        #    qsize = self._shared_qsize.value
+
+        #publisher = get_metrics_service_instance()
+        #publisher.add_gauge(self._metric, qsize)
 
     def recv(self, *args, **kwargs) -> SampleMessage:
         msg = super().recv(*args, **kwargs)
-
-        with self._shared_qsize.get_lock():
-            self._shared_qsize.value = max(0, self._shared_qsize.value - 1)
-            qsize = self._shared_qsize.value
-
         publisher = get_metrics_service_instance()
-        publisher.add_gauge(self._metric, qsize)
+        publisher.add_count(self._dequeued_metric, 1)
+        #with self._shared_qsize.get_lock():
+        #    self._shared_qsize.value = max(0, self._shared_qsize.value - 1)
+        #    qsize = self._shared_qsize.value
+
+        #publisher = get_metrics_service_instance()
+        #publisher.add_gauge(self._metric, qsize)
         return msg
