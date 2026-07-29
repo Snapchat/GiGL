dc14ea30238e3b8c6a978f07b585e919d6d849eb
mkolodner <mkolodner@snapchat.com>
2026-07-29 22:19:53 +0000

    Expose typed PPR sampler API

--- gigl/distributed/utils/dist_sampler.py
+++ gigl/distributed/utils/dist_sampler.py
@@ -85,6 +85,7 @@ def create_dist_sampler(
             enable_residual_topup=sampler_options.enable_residual_topup,
             max_fetch_iterations=sampler_options.max_fetch_iterations,
             num_neighbors_per_hop=sampler_options.num_neighbors_per_hop,
+            typed_channel_ratios=sampler_options.typed_channel_ratios,
             degree_tensors=degree_tensors,
         )
     else:
