1b46d6c2577c0b7c21d2c8c4ae786911be27a9cc
jchmura <jchmura@snapchat.com>
2026-07-30 16:07:11 +0000

    Record log level to user

--- gigl/common/logger.py
+++ gigl/common/logger.py
@@ -21,6 +21,8 @@ class Logger(logging.LoggerAdapter):
         extra (Optional[dict[str, Any]]): Extra information to be added to the log message.
     """
 
+    _DID_ALERT_FOR_LOG_LEVEL: bool = False
+
     def __init__(
         self,
         logger: Optional[logging.Logger] = None,
@@ -28,22 +30,32 @@ class Logger(logging.LoggerAdapter):
         log_to_file: bool = False,
         extra: Optional[dict[str, Any]] = None,
     ):
+        gigl_debug = is_env_flag_enabled(GIGL_DEBUG)
+        if gigl_debug:
+            log_level = logging.DEBUG
+        else:
+            log_level = logging.INFO
+
         if logger is None:
             logger = logging.getLogger(name)
-            self._setup_logger(logger, name, log_to_file)
+            self._setup_logger(logger, name, log_to_file, log_level)
 
         super().__init__(logger, extra or {})
 
+        if not Logger._DID_ALERT_FOR_LOG_LEVEL:
+            Logger._DID_ALERT_FOR_LOG_LEVEL = True
+            level_name = logging.getLevelName(log_level)
+            self.info(f"{GIGL_DEBUG}={gigl_debug}, using log level {level_name}")
+
     def _setup_logger(
-        self, logger: logging.Logger, name: Optional[str], log_to_file: bool
+        self,
+        logger: logging.Logger,
+        name: Optional[str],
+        log_to_file: bool,
+        log_level: int,
     ) -> None:
         handler: logging.Handler
         if not logger.handlers:
-            if is_env_flag_enabled(GIGL_DEBUG):
-                log_level = logging.DEBUG
-            else:
-                log_level = logging.INFO
-
             if os.getenv("GAE_APPLICATION") or os.environ.get(
                 "KUBERNETES_SERVICE_HOST"
             ):
