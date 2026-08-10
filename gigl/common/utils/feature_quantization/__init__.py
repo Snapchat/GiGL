3fe1a4f15b8f615b99ef299be85b5d05df39d638
jchmura <jchmura@snapchat.com>
2026-08-10 19:50:13 +0000

    Guard against duplicate feature keys specified for quantization

--- gigl/common/utils/feature_quantization/__init__.py
+++ gigl/common/utils/feature_quantization/__init__.py
@@ -1,5 +1,3 @@
 """Utilities for node feature quantization in GiGL."""
 
-from typing import Final
-
-SUPPORTED_QUANTIZATION_BITS: Final[tuple[int, ...]] = (1, 2, 4, 8)
+from gigl.common.utils.feature_quantization.constants import SUPPORTED_QUANTIZATION_BITS
---
+++ gigl/common/utils/feature_quantization/constants.py
@@ -0,0 +1,3 @@
+from typing import Final
+
+SUPPORTED_QUANTIZATION_BITS: Final[tuple[int, ...]] = (1, 2, 4, 8)
