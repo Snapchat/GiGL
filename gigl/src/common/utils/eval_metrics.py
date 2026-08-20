098eea052e365683f3710b78563fe3d498ae5ae5
jchmura <jchmura@snapchat.com>
2026-08-20 23:54:06 +0000

    Add batch dim optional to hits and mrr eval

--- gigl/src/common/utils/eval_metrics.py
+++ gigl/src/common/utils/eval_metrics.py
@@ -3,10 +3,10 @@ from jaxtyping import Float, Float32, Int64
 
 
 def hit_rate_at_k(
-    pos_scores: Float[torch.FloatTensor, "positives"],
-    neg_scores: Float[torch.FloatTensor, "negatives"],
-    ks: Int64[torch.LongTensor, "requested_ks"],
-) -> Float32[torch.FloatTensor, "requested_ks"]:
+    pos_scores: Float[torch.FloatTensor, "*batch positives"],
+    neg_scores: Float[torch.FloatTensor, "*batch negatives"],
+    ks: Int64[torch.LongTensor, "*batch requested_ks"],
+) -> Float32[torch.FloatTensor, "*batch requested_ks"]:
     """Computes Hit Rate @ K metrics for various Ks, evaluating 1+ positives against 1+ negatives.
 
     Args:
@@ -50,8 +50,8 @@ def hit_rate_at_k(
 
 
 def mean_reciprocal_rank(
-    pos_scores: Float[torch.FloatTensor, "positives"],
-    neg_scores: Float[torch.FloatTensor, "negatives"],
+    pos_scores: Float[torch.FloatTensor, "*batch positives"],
+    neg_scores: Float[torch.FloatTensor, "*batch negatives"],
 ) -> Float32[torch.FloatTensor, ""]:
     """Computes Mean Reciprocal Rank (MRR), evaluating 1+ positives against 1+ negatives.
 
